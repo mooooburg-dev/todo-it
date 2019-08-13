ed1afc80bfced649d9f91f948087ff0750b30ab9
mooooburg <jeongmupark@gmail.com>
2019-08-13 19:20:51 +0900

    뷰 모달 추가하기

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,41 +1,36 @@
 <template>
     <section>
-      <ul>
-          <li v-for="(todoItem, index) in todoItems" class="shadow">
+      <transition-group name="list" tag="ul">
+          <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
             <i class="checkBtn fas fa-check" aria-hidden="true"></i>
             {{ todoItem }}
             <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
               <i class="far fa-trash-alt" aria-hidden="true"></i>
             </span>
           </li>
-      </ul>
+      </transition-group>
     </section>
 </template>
 
 <script>
   export default{
-    data(){
-      return {
-        todoItems: []
-      }
-    },
-    created(){
-      if(localStorage.length > 0){
-        for(var i=0 ; i<localStorage.length; i++){
-          this.todoItems.push(localStorage.key(i));
-        }
-      }
-    },
+    props: ['propsdata'],
     methods: {
       removeTodo(todoItem, index){
-        localStorage.removeItem(todoItem);
-        this.todoItems.splice(index, 1);
+        this.$emit('removeTodo', todoItem, index);
       }
     }
   }
 </script>
 
 <style>
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to{
+    opacity: 0;
+    transform: translateY(-30px);
+  }
   ul{
     list-style-type: none;
     padding-left: 0px;
