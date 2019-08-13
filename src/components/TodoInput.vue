bef96a1c02a4f0e7a84e4c4a920792611635b941
mooooburg <jeongmupark@gmail.com>
2019-08-13 16:40:55 +0900

    입력받은 텍스트 로컬 스토리지에 저장하기

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,13 +1,24 @@
 <template>
     <div>
-      input
+      <input type="text" v-model="newTodoItem" />
+      <button v-on:click="addTodo">추가</button>
     </div>
 </template>
 
 <script>
-  export default{
-
+export default {
+  data(){
+    return{
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo: function(){
+      // console.log(this.newTodoItem);
+      localStorage.setItem(this.newTodoItem, this.newTodoItem); // setItem()는 로컬 스토리지에 데이터를 추가하는 API
+    }
   }
+}
 </script>
 
 <style>
